528775304a4fb0cb78fead5aac67346bc6a6ef41
Jamie <zichu_zark@msn.com>
2023-02-03 16:39:56 +0000

    Actually fixed the image paths and deployed a new build

--- src/app/data/services.ts
+++ src/app/data/services.ts
@@ -4,11 +4,11 @@ export const Services: Service[] = [
     {
         id: 'polish',
         title: 'Polish',
-        imageSrc: '/assets/images/nail-polish.svg'
+        imageSrc: 'assets/images/nail-polish.svg'
     },
     {
         id: 'filing',
         title: 'Nail Filing',
-        imageSrc: '/assets/images/nail-file.svg'
+        imageSrc: 'assets/images/nail-file.svg'
     }
 ]
